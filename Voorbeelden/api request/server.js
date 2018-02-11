cd9796f6236a7fb568901d83803afbd020dd8246
Unknown <guido313@hotmail.nl>
2018-02-11 15:05:57 +0100

    Api request vb

--- Voorbeelden/api request/server.js
+++ Voorbeelden/api request/server.js
@@ -3,44 +3,18 @@ const https = require("https");
 const fs = require("fs");
 
 const options = {
-    hostname: "https://en.wikipedia.org/",
-    port: 443,
-    path: "w/api/Elon_Musk",
-    method: "GET"
+    host: 'www.cryptocompare.com',
+    port: '',
+    path: '/api/data/coinlist',
+    method: 'GET',
 };
 
-/*
-const req = https.request(options, (res) => {
 
-    const responseBody = "";
-
-    console.log('\x1b[32m Response from server started. \x1b[37m');
-    console.log('\x1b[32m Server status: \x1b[37m', res.statusCode);
-    console.log('\x1b[32m Response headers: \x1b[37m    \n', res.headers);
-
-    //res.setEncoding("UTF-8");
-
-    res.on('data', (datachunk) => {
-        //console.log(datachunk);
-        process.stdout.write(datachunk);
-    });
-
-    res.on("end", function () {
-        fs.writeFile("george-washington.html", responseBody, function (err) {
-            if (err) {
-                throw err;
-            }
-            console.log("File Downloaded");
-        });
-    });
-});*/
-
-
-
+var responseBody = "";
 
 const req = https.request(options, (res) => {
 
-    var responseBody = "";
+
 
     console.log('\x1b[32m Response from server started. \x1b[37m');
     console.log('\x1b[32m Server status: \x1b[37m', res.statusCode);
@@ -64,7 +38,6 @@ const req = https.request(options, (res) => {
         });
     });
 
-
 });
 
 /*req.on('error', (e) => {
@@ -72,3 +45,10 @@ const req = https.request(options, (res) => {
 });*/
 
 req.end();
+
+
+
+
+
+
+
